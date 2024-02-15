c3b68ad0ed0654730bd2826a27699050b3dab2fc
Vaishnavi Bansode <vaishnavibansode24@gmail.com>
2024-02-15 14:35:14 +0530

    changed the color of heading

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function fetchWeatherCity(cityName){
 	let humidity = document.getElementById("humidity");
 	humidity.innerHTML = data.main.humidity+ "g/kg";
 	let temp = document.getElementById("temp");
-	temp.innerHTML = data.main.temp + "°C"
+	temp.innerHTML = Math.round((data.main.temp)-273.15) + "°C";
 	console.log(data)
 }
 //demo()
