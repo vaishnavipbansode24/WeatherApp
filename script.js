3839a5de7f3639a4a2cb6f5ac8a9a5ce625f91e5
Vaishnavi Bansode <vaishnavibansode24@gmail.com>
2024-02-15 13:47:45 +0530

    Added  background image and all the changes related to the display of the temperature

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ async function fetchWeatherCity(cityName){
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=4398bc447fb7e34932a28a5d9eae4b34`;
 	const response = await fetch(url)
 	const data = await response.json();
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML= data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+ "g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = data.main.temp + "°C"
 	console.log(data)
 }
 //demo()
